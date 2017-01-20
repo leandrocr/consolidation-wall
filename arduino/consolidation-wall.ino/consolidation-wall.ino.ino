e78f343c5b49d9ddb885a316f958b38c795c55c8
Leandro Repolho <leandro.repolho@theiconic.com.au>
2017-01-20 16:37:29 +1100

    Clear code to allow ProjectX Demo Faster

--- arduino/consolidation-wall.ino/consolidation-wall.ino.ino
+++ arduino/consolidation-wall.ino/consolidation-wall.ino.ino
@@ -1,42 +1,39 @@
-int aa = 11;
-int ab = 12;
-int ac = 13;
 int ba = 8;
 int bb = 9;
-int bc = 10;
 int consolidationWallSlot = 0;
 
 void setup() {
   Serial.begin(9600);
   
-  pinMode(aa, OUTPUT);
-  pinMode(ab, OUTPUT);
-  pinMode(ac, OUTPUT);
   pinMode(ba, OUTPUT);
   pinMode(bb, OUTPUT);
-  pinMode(bc, OUTPUT);
 }
 
 void loop() {
+  
+  
   if (Serial.available() > 0) {
     consolidationWallSlot = Serial.parseInt();
+
+    if (consolidationWallSlot == 1) {
+      consolidationWallSlot = 8;
+    } else {
+      consolidationWallSlot = 9;
+    }
+    
 
     turnOn(consolidationWallSlot);
   }
 
-  delay(1000);
+  delay(10);
 }
 
 void turnOn(int ledNumber) {
-  digitalWrite(aa, LOW);
-  digitalWrite(ab, LOW);
-  digitalWrite(ac, LOW);
   digitalWrite(ba, LOW);
   digitalWrite(bb, LOW);
-  digitalWrite(bc, LOW);
 
   digitalWrite(ledNumber, HIGH);
 
-  Serial.print(ledNumber);
+  
 }
 
